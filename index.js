4eb6b6c94993c95eb198f9df6a759eb9e540d473
Zoey Cluff <zoey.cluff@gmail.com>
2018-10-05 08:05:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowercase() === lowercase) {
+  if (string === lowercase) {
     return "I can't hear you!"
   } 
 }
