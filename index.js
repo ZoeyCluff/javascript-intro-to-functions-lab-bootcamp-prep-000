5bc2181a7b9c18536ab6064c7c7bd3e9b86e210e
Zoey Cluff <zoey.cluff@gmail.com>
2018-10-05 08:04:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   } else if (string === "I love you, grandma.") {
     return "I love you, too."
-  } else  {
+  } else if  ( {
     return "YES INDEED!"
 }
 }
